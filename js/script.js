062613c04778bdb911e6ed85e98f7dcf6d09751d
mikzanco <mikyzancolo97@gmail.com>
2022-10-18 16:16:50 +0200

    add comments

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 // Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo for e con un ciclo while (potete farlo nello stesso file o in due file separati).
 
 
-
+// creo un array che contiene le cose che dovrò poi comprare
 const listaSpesa = ['carote', 'biscotti', 'nuetella', 'banane', 'pasta'];
 
 // console.log(listaSpesa);
@@ -13,13 +13,17 @@ const listaSpesa = ['carote', 'biscotti', 'nuetella', 'banane', 'pasta'];
 
 // Stampo in pagina gli elemnti della lista indivisualemnet con il ciclo for
 
-// for (let i = 0; i < listaSpesa.length; i++) {
-//     const li = document.createElement("li");
-//     let spesa = listaSpesa[i];
-//     console.log(spesa);
-//     li.innerHTML = spesa;
-//     document.body.append(li);
-// }
+for (let i = 0; i < listaSpesa.length; i++) {
+    // creo l'eleemnto li
+    const li = document.createElement("li");
+    // cre la lista "spesa" utilizzando il ciclo for e quindi l'elemento i
+    let spesa = listaSpesa[i];
+    console.log(spesa);
+    // con innerHTML posiziono la mia lista spesa nell'elemnto li che ho creato
+    li.innerHTML = spesa;
+    // con append attacco gli li creati alla pagina web
+    document.body.append(li);
+}
 
 // -----------------------------------------------------------------------------------------------
 
@@ -32,15 +36,15 @@ let c = 0;
 let spesa = 0;
 
 // il ciclo while mi permette di stampare in pagina tutti gli elementi del mio array
+
 while (c < listaSpesa.length) {
+
     const li = document.createElement("li");
     let spesa = listaSpesa[c];
     console.log(spesa);
     li.innerHTML = spesa;
-    
     c++;
     document.body.append(li);
-
 }
 
 
