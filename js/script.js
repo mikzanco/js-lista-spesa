a53efc2f4b1f438e2ae0a3acee844576aa3cfcbc
mikzanco <mikyzancolo97@gmail.com>
2022-10-18 14:13:05 +0200

    first commit: collegato js al html crreato h1 e creato array (listaSpesa)

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Abbiamo un array di alimenti.
+// Dobbiamo stampare in pagina una lista contenente tutti gli alimenti.
+// Consegna:
+// Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo for e con un ciclo while (potete farlo nello stesso file o in due file separati).
+
+
+
+const listaSpesa = ['carote', 'biscotti', 'nuetella', 'banane', 'pasta',];
+
+console.log(listaSpesa);
